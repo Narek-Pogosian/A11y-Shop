47a3993218c7a05e7664579489587d4514b7e23c
Narek Pogosian <narekpogosian2@gmail.com>
2024-04-29 12:16:42 +0200

    edit product-card to use single link around title and cover card with after element

--- src/app/(customer)/shop/_components/product-card.tsx
+++ src/app/(customer)/shop/_components/product-card.tsx
@@ -1,6 +1,9 @@
 import { type Product } from "@/server/db/schema";
+import { ShoppingBag } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { currency } from "@/lib/utils";
 
 interface ProductCardProps {
   product: Product;
@@ -8,7 +11,7 @@ interface ProductCardProps {
 
 export function ProductCard({ product }: ProductCardProps) {
   return (
-    <article>
+    <article className="product-card relative rounded">
       <div className="aspect-video overflow-hidden rounded">
         {product.image ? (
           // eslint-disable-next-line @next/next/no-img-element
@@ -21,9 +24,25 @@ export function ProductCard({ product }: ProductCardProps) {
           <div className="h-full w-full bg-accent"></div>
         )}
       </div>
-      <h2>
-        <Link href="/">{product.name}</Link>
-      </h2>
+      <div className="flex items-center justify-between px-1 pb-1 pt-2">
+        <div className="flex flex-col font-semibold">
+          <h2>
+            <Link
+              href="/"
+              className="text-lg outline-none after:absolute after:inset-0"
+            >
+              {product.name}
+            </Link>
+          </h2>
+          <span className="text-sm text-muted-foreground">
+            {currency(product.price)}
+          </span>
+        </div>
+        <Button size="icon" className="relative">
+          <span className="sr-only">Add to Cart</span>
+          <ShoppingBag className="size-6" />
+        </Button>
+      </div>
     </article>
   );
 }
