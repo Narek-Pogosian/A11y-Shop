36ce21560eba4e2a5bd7bddcb2caaa69d9b6c2d3
Narek Pogosian <narekpogosian2@gmail.com>
2024-05-04 00:49:09 +0200

    remove product modal route

--- src/app/(customer)/shop/[slug]/loading.tsx
+++ src/app/(customer)/shop/[slug]/loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react";
 
 function loading() {
   return (
-    <div className="pt-20 text-center">
+    <div className="flex justify-center pt-32">
       <Loader className="size-14 animate-spin" />
     </div>
   );
