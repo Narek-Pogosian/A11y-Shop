bc95b4f70f0226c413474a3fffb29378135fceaa
Narek Pogosian <narekpogosian2@gmail.com>
2024-05-05 20:29:18 +0200

    feat: show check icon instead cart after adding product, reverts with timeout

--- src/app/(customer)/shop/_components/product-card-add-to-cart.tsx
+++ src/app/(customer)/shop/_components/product-card-add-to-cart.tsx
@@ -1,26 +1,36 @@
 "use client";
 
 import { type Product } from "@/server/db/schema";
-import { ShoppingBag } from "lucide-react";
+import { Check, ShoppingBag } from "lucide-react";
 import { useCart } from "@/hooks/use-cart";
 import { Button } from "@/components/ui/button";
 import { announce } from "@react-aria/live-announcer";
+import { useState } from "react";
 
 interface ProductCardAddProps {
   product: Product;
 }
 
 function ProductCardAddToCart({ product }: ProductCardAddProps) {
+  const [hasAdded, setHasAdded] = useState(false);
   const { addToCart } = useCart();
 
   function handleAddToCart() {
+    setHasAdded(true);
     addToCart({ product, quantity: 1, productId: product.id });
     announce(`Added one ${product.name} to cart`, "assertive");
+    setTimeout(() => {
+      setHasAdded(false);
+    }, 800);
   }
 
   return (
     <Button size="icon" className="relative size-8" onClick={handleAddToCart}>
-      <ShoppingBag className="size-5" aria-hidden />
+      {!hasAdded ? (
+        <ShoppingBag className="size-5" aria-hidden />
+      ) : (
+        <Check className="size-5" aria-hidden />
+      )}
       <span className="sr-only">Add to cart.</span>
     </Button>
   );
